ad9cc6939c91f6a3dafe0f13a6fc86709b142509
Abinesh S <abineshsiva191@gmail.com>
2022-04-09 22:15:06 +0530

    chain

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
 const contractAddress = "0xA390483090aA6f3Bfb1F550CebB4A3C96Fa70e9A"; //Replace with your own contract address
-const chain = 'polygon'; // rinkeby or polygon
+const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Nohelps NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
